b190ffb99dd3f5c2964ae5655a22102aa555904f
Igor Spirin <i_spirin@inbox.ru>
2024-10-07 23:28:30 +0300

    Теперь с комментариями! Красивый код. FPS и масочки

--- DenseOF.py
+++ DenseOF.py
@@ -23,25 +23,77 @@ import sys
 
 
 def draw_flow(img_shape, flow, step=14):  # step=16
+    """Возвращает слой с векторами движения пикселей"""
     h, w = img_shape
     img_bgr = np.zeros((h, w, 3), np.uint8)
     y, x = np.mgrid[step / 2:h:step, step / 2:w:step].reshape(2, -1).astype(int)
     fx, fy = flow[y, x].T
 
+    global lines
+    # сборка массива по столбцам и строкам, на выходе размер (999, 2, 2)
     lines = np.vstack([x, y, x - fx, y - fy]).T.reshape(-1, 2, 2)
-    lines = np.int32(lines + 0.5)
+    lines = np.int32(lines + 0.5)  # математическое округление
 
     cv2.polylines(img_bgr, lines, False, (0, 255, 0))
 
     # точки н-нада?
-    if True:
+    if 1:
         for (x1, y1), (_x2, _y2) in lines:
             cv2.circle(img_bgr, center=(x1, y1), radius=1, color=(0, 255, 0),  thickness=-1)
 
     return img_bgr
 
 
+def draw_grid(img_shape, step=20, colored_cross=False,
+              viewing_angle_rect=False, cross=False, grid=False, blinds=False) -> np.ndarray:
+    """Возвращает слой с сеткой"""
+    h, w = img_shape
+    img_bgr = np.zeros((h, w, 3), np.uint8)
+    x_lines = np.int32([[[i, 0], [i, h]] for i in range(step, w, step)])
+    y_lines = np.int32([[[0, i], [w, i]] for i in range(step, h, step)])
+
+    if grid:
+        # сетка пикселей, а то я не вижу, куда что рисовать BGR
+        cv2.polylines(img_bgr, x_lines, False, (100, 100, 100), 1)
+        cv2.polylines(img_bgr, y_lines, False, (100, 100, 100), 1)
+    if cross:
+        # Центр кадра
+        cv2.polylines(img_bgr, [np.int32([[half_width, 0], [half_width, height]])], False, (0, 0, 255), 1)
+        cv2.polylines(img_bgr, [np.int32([[0, half_height], [width, half_height]])], False, (0, 0, 255), 1)
+    if colored_cross:
+        # Цветной крест
+        cv2.line(img_bgr, (0, half_height), (15, half_height), (0, 0, 255), 5)  # Красный
+        cv2.line(img_bgr, (half_width, 0), (half_width, 15), (0, 255, 0), 5)  # Зелёный
+        cv2.line(img_bgr, (width, half_height), (width-15, half_height), (255, 200, 170), 5)  # Голубой
+        cv2.line(img_bgr, (half_width, height), (half_width, height-15), (255, 100, 100), 5)  # Синий
+    if blinds:
+        # blinds Нужно ?закрыть? зону, где видны подвижные винты
+        cv2.rectangle(img_bgr, (620, height-200), (-1, height), (0, 0, 255,), 1)
+        cv2.rectangle(img_bgr, (width-620, height-200), (width, height), (0, 0, 255), 1)
+        # cv2.ellipse(img_bgr, (width, height-70), (670, 150), 0, 160, 270, (0, 0, 255), 3)
+        # cv2.ellipse(img_bgr, (0, height-70), (670, 150), 0, 270, 380, (0, 255, 0), 3)
+    if viewing_angle_rect:
+        # считаем синий прямоугольник угла обзора
+        if viewing_angle > viewing_angle_req:  # если он нужен
+            width_res = round(width * viewing_angle_req / viewing_angle)
+            height_res = round(height * viewing_angle_req / viewing_angle)
+            rect_x0 = round((width - width_res) / 2)
+            rect_y0 = round((height - height_res) / 2)
+            rect_x1 = rect_x0 + width_res
+            rect_y1 = rect_y0 + height_res
+        else:  # прячем за границы, если не нужен
+            rect_x0 = -1
+            rect_y0 = -1
+            rect_x1 = width
+            rect_y1 = height
+        cv2.rectangle(img_bgr, (rect_x0, rect_y0), (rect_x1, rect_y1), (255, 0, 0), 1)  # прямоугольник обзора 60 градусов
+
+    return img_bgr
+
+
 def draw_hsv(flow):
+    """Возвращает слой с радугой в цветовом пространстве hsv,
+    где цвет - направление вектора, интенсивность - длинна вектора"""
     h, w = flow.shape[:2]
     fx, fy = flow[:, :, 0], flow[:, :, 1]
 
@@ -57,11 +109,11 @@ def draw_hsv(flow):
     return bgr
 
 
-# TODO настройка
-add_flow = True
+# МЕНЮ НАСТРОЙКИ
+add_flow = False
 add_hsv = False
 show_hsv = False
-match 1:
+match 1:  # СЮДА МЕНЯТЬ
     case 0:
         cap = cv2.VideoCapture(0)
         start_frame = 100  #
@@ -79,6 +131,7 @@ match 1:
         viewing_angle = 361
         print("\\(-_-)/")
         sys.exit()
+# расчёт постоянных, вынесенных из цикла
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 video_fps = cap.get(cv2.CAP_PROP_FPS)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -86,88 +139,79 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 half_width = int(width/2)
 half_height = int(height/2)
 viewing_angle_req = 60
-if viewing_angle > viewing_angle_req:
-    width_res = round(width * viewing_angle_req/viewing_angle)
-    height_res = round(height * viewing_angle_req/viewing_angle)
-    rect_x0 = round((width - width_res)/2)
-    rect_y0 = round((height - height_res)/2)
-    rect_x1 = rect_x0 + width_res
-    rect_y1 = rect_y0 + height_res
-else:
-    rect_x0 = -1
-    rect_y0 = -1
-    rect_x1 = width
-    rect_y1 = height
+frame_queue = []
 
+# инструкция
 print('Пробел - пауза, 1 - добавить стрелочки, 2 - добавить HSV, 3 - окно HSV, Q/Esc - закрыть окна')
 print('Кадров', length, 'Ширина', width, 'Высота', height, 'FPS', video_fps)
 print('Запуск с', start_frame)
-cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)  # frame_number
 
-suc, prev = cap.read()
+# Начало кода для прототипа.
+cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)  # номер кадра, с которого начнём
+suc, prev = cap.read()  # захват кадра
 if not suc:
     print('Картинки нет')
-prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
 
+# Цикл покадровой итерации
 go = True
+end_cycle = None
+start_video = time.time()
 while go:
-    # start time to calculate FPS
-    start = time.time()
+    # начальное время для расчёта FPS
+    start_cycle = time.time_ns()  # time.time()
 
     suc, img = cap.read()
     if not suc:
+        end_video = time.time()
         cv2.waitKey(0)
+        print(int(start_video - end_video), "секунд показа")
         print("Спасибо за внимание")
         break
+
+    # очередь предыдущих кадров
+    if not len(frame_queue) > 5:
+        frame_queue.append((img, int(cap.get(cv2.CAP_PROP_POS_FRAMES))))
+    else:
+        frame_queue.pop(0)
+        frame_queue.append((img, int(cap.get(cv2.CAP_PROP_POS_FRAMES))))
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Сравнение кадров попиксельно
+    # print(f"{cap.get(cv2.CAP_PROP_POS_FRAMES):.0f} кадр", abs(sum(sum(prev_gray - gray))))
 
-    # Сравнение кадров?
-    # if not (np.bitwise_xor(prevgray, gray).any()):
-    #     print(f"{cap.get(cv2.CAP_PROP_POS_FRAMES):.0f} кадр")
-
+    # считаем поток, только если его нужно отображать (буст)
     if add_flow or add_hsv or show_hsv:
-        flow = cv2.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3,
+        flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3,
                                             5, 1.2, 0)
 
-    prevgray = gray
+    prev_gray = gray
 
-    gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    output_bgr_gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 
-    # print(f"{fps:.2f} FPS")
-
+    # сложение слоёв для формирования картинки
     if add_flow:
-        gray = cv2.add(gray, draw_flow(gray.shape[:2], flow))
+        output_bgr_gray = cv2.add(output_bgr_gray, draw_flow(output_bgr_gray.shape[:2], flow))
     if add_hsv:
-        gray = cv2.add(gray, draw_hsv(flow))
+        output_bgr_gray = cv2.add(output_bgr_gray, draw_hsv(flow))
     if show_hsv:
         cv2.imshow('flow HSV', draw_hsv(flow))
 
-    end = time.time()
-    fps = 1 / (end - start)
-    if fps > video_fps:
-        time.sleep(1/video_fps - (end - start))
-        end = time.time()
-        fps = 1 / (end - start)
-
-
-    cv2.rectangle(gray, (rect_x0, rect_y0), (rect_x1, rect_y1), (255, 0, 0), 1)
-    cv2.putText(gray, f"{fps:.2f} FPS", (20, 30), cv2.FONT_HERSHEY_COMPLEX, 1,
-                (0, 255, 0), 2)
-    cv2.putText(gray, f"{cap.get(cv2.CAP_PROP_POS_FRAMES):.0f} кадр", (20, 70), cv2.FONT_HERSHEY_COMPLEX,
+    # Конец кода для прототипа. Конечное время вычислений, большинство из которых планируется делать на устройстве
+    # Текстовая информация в углу
+    cv2.putText(output_bgr_gray, f"{cap.get(cv2.CAP_PROP_POS_FRAMES):.0f} кадр", (20, 70), cv2.FONT_HERSHEY_COMPLEX,
                 1, (0, 255, 0), 2)
-    cv2.putText(gray, f"{cap.get(cv2.CAP_PROP_POS_MSEC) / 1000:.3f} секунд", (20, 110),
-                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0),
-                2)
-    cv2.ellipse(gray, (0, height), (300, height), 0, 90, 180, (255, 255, 255), 3)
-    cv2.ellipse(gray, (width, height), (0, height), 0, 0, 90, (255, 255, 255), 3)
-    cv2.line(gray, (0, half_height), (15, half_height), (0, 0, 255), 5)  # Красный
-    cv2.line(gray, (half_width, 0), (half_width, 15), (0, 255, 0), 5)  # Зелёный
-    cv2.line(gray, (width, half_height), (width-15, half_height), (255, 200, 170), 5)  # Голубой
-    cv2.line(gray, (half_width, height), (half_width, height-15), (255, 100, 100), 5)  # Синий
+    cv2.putText(output_bgr_gray, f"{cap.get(cv2.CAP_PROP_POS_MSEC) / 1000:.3f} секунд", (20, 110),
+                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(output_bgr_gray, f"{time.time() - start_video:.3f} с время показа", (20, 150),
+                cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 255, 0), 1)
 
-    cv2.imshow('flow', gray)
-    # cv2.resizeWindow('flow', 900, 900)
+    # разметка кадра
+    output_bgr_gray = cv2.add(output_bgr_gray, draw_grid((height, width), 20,
+                                                         colored_cross=True, viewing_angle_rect=True, cross=True,
+                                                         grid=False, blinds=True))
 
+    # Управление с клавиатуры
     key = cv2.waitKey(1)
     if key == ord(' '):
         while True:
@@ -186,6 +230,27 @@ while go:
         # print('HSV flow visualization is', ['off', 'on'][show_hsv])
     if (key == ord('q')) or (key == ord('й')) or (key == 27):
         break
+    end_cycle = time.time_ns()  # time.time()
 
+    # Подсчёт FPS
+    end_calculations = time.time_ns()  # time.time()
+    fps = 1 / ((end_calculations - start_cycle)/(10**9))
+    if end_cycle is None:
+        end_cycle = start_cycle + 0.01
+    if fps > video_fps:
+        calc_time = (end_cycle - start_cycle)/(10**9)
+        # time.sleep(1 / (video_fps + calc_time))
+        end_calculations = time.time_ns()  # time.time()
+        if calc_time != 0:
+            fps = 1 / calc_time
+        else:
+            fps = 99
+
+    # print(f"{fps:.2f} FPS")
+    cv2.putText(output_bgr_gray, f"{fps:.2f} FPS", (20, 30), cv2.FONT_HERSHEY_COMPLEX, 1,
+                (0, 255, 0), 2)
+    # Вывод результирующего кадра
+    cv2.imshow('flow', output_bgr_gray)
+    # cv2.resizeWindow('flow', 900, 900)
 cap.release()
 cv2.destroyAllWindows()
